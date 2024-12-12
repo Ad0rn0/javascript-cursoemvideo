39b6aa5be8387baaed1e9e428ba0810584833e9a
Thiago Adorno | Xpert Pack BA <ad0rn0code@gmail.com>
2024-12-12 17:45:44 -0300

    Finish E Module

--- ModuloE/aula14ex/ex016/script.js
+++ ModuloE/aula14ex/ex016/script.js
@@ -2,6 +2,7 @@ function count() {
     var startNumber = document.getElementById("nstart")
     var endNumber = document.getElementById("nend")
     var skipNumber = document.getElementById("nskip")
+    var nextNumber = 0
 
     var result = document.querySelector("div#res")
 
@@ -12,16 +13,14 @@ function count() {
             alert("Invalid Skip. Considering skip = 1")
             skipNumber.value = 1
         } 
-        
+
         result.innerHTML = ''
-        for (var i = 1; i <= endNumber.value; i++) {
-            console.log(`Teste ${i}`)
+        for (var i = Number(startNumber.value); i <= endNumber.value; i+=Number(skipNumber.value)) {
+            nextNumber = i + Number(skipNumber.value)
             result.innerHTML += i
-            if (i != endNumber.value){
-                result.innerHTML += ' -> '
+            if (nextNumber <= endNumber.value){
+                    result.innerHTML += ' -> '
             }
-            
         }
-        
     }
 }
--- ModuloE/aula14ex/ex017/script.js
+++ ModuloE/aula14ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert("oi")
+function generate() {
+    var num = document.querySelector('input#num')
+    var tab = document.getElementById('tab')
+
+    if (num.value === "") {
+        window.alert('Please input a valid number!')
+    } else{
+        tab.innerHTML = ''
+        var number = Number(num.value)
+        for (var i = 1; i <= 10; i++){
+            var opc = document.createElement('option')
+            opc.text = `${number} X ${i} = ${number*i}`
+            tab.value = `opc${i}`
+            tab.appendChild(opc)
+        }
+    }
+}
